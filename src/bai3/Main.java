2ad86f75b6ca4dc640305a0db5ba645eee9b3491
An Peter <kieuthaian9x@gmail.com>
2026-01-05 01:34:31 +0700

    Done exersice 3

--- src/bai3/Main.java
+++ src/bai3/Main.java
@@ -1,4 +1,13 @@
 package bai3;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(
+            "Học viên: \"Nguyễn Văn A\"\n" +
+            "Lớp:\t  Java Backend\n" +
+            "Mục tiêu:\n" +
+            "\t\t+ Nắm vững Array\n" +
+            "\t\t+ Thành thạo String"
+        );
+    }
 }
